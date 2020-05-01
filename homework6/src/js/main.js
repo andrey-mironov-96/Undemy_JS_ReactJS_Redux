66ec77651045640d6cbe85ef2b0815b6f42ff464
Andrey <andrey-mironov-96@mail.ru>
2020-05-01 22:32:45 +0400

    Выполнено 1 и 2 задания

---
+++ homework6/src/js/main.js
@@ -0,0 +1,25 @@
+//homework 6
+
+//task1
+let task1 = () =>{
+    let startCall = document.getElementById('start');
+    console.log(startCall);
+};
+task1();
+
+let task2 = () =>{
+    let budgetValue = document.getElementsByClassName('budget-value'),
+        levelValue = document.getElementsByClassName('level-value'),
+        expensesValue = document.getElementsByClassName('expenses-value'),
+        optionalexpensesValue = document.getElementsByClassName('optionalexpenses-value'),
+        incomeValue = document.getElementsByClassName('income-value'),
+        monthsavingsValue = document.getElementsByClassName('monthsavings-value'),
+        yearsavingsValue = document.getElementsByClassName('yearsavings-value'),
+        yearValue = document.getElementsByClassName('year-value'),
+        monthValue = document.getElementsByClassName('month-value'),
+        dayValue = document.getElementsByClassName('day-value');
+                  
+        
+    
+};
+task2();
