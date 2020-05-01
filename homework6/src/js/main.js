aeb970a88d765de11ead74bf016449d0840fac60
Andrey <andrey-mironov-96@mail.ru>
2020-05-01 23:09:03 +0400

    Домашнее задание номер 7 успешно завершено

--- homework6/src/js/main.js
+++ homework6/src/js/main.js
@@ -17,9 +17,47 @@ let task2 = () =>{
         yearsavingsValue = document.getElementsByClassName('yearsavings-value'),
         yearValue = document.getElementsByClassName('year-value'),
         monthValue = document.getElementsByClassName('month-value'),
-        dayValue = document.getElementsByClassName('day-value');
-                  
-        
-    
+        dayValue = document.getElementsByClassName('day-value'); 
 };
 task2();
+
+let task3 = () =>{
+    let expensesItem = document.getElementsByClassName('expenses-item');
+};
+task3();
+let task4 = ()=>{
+let expensesItemBtn1 = document.getElementsByTagName('button')[0],
+    expensesItemBtn2 = document.getElementsByTagName('button')[1],
+    expensesItemBtn3 = document.getElementsByTagName('button')[2];
+    console.log(expensesItemBtn1);
+    console.log(expensesItemBtn2);
+    console.log(expensesItemBtn3);
+    
+};
+task4();
+
+let task5 = ()=>{
+    let optionalexpensesItem = document.querySelectorAll('.optionalexpenses-item');
+    
+};
+
+let task6 = ()=>{
+    let  chooseIncome = document.querySelector('.choose-income'),
+         savings = document.querySelector('#savings'),
+         chooseSum = document.querySelector('.choose-sum'),
+         choosePercent = document.querySelector('.choose-percent'),
+         yearValue = document.querySelector('.year-value'),
+         monthValue = document.querySelector('.month-value'),
+         dayValue = document.querySelector('.day-value');
+    console.log(chooseIncome);
+    console.log(savings);
+    console.log(chooseSum);
+    console.log(choosePercent);
+    console.log(yearValue);
+    console.log(monthValue);
+    console.log(dayValue);
+};
+task6();
+
+
+
