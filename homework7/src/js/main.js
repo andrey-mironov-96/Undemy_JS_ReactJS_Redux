f49987e4558a22650822b3d8d144dfabb1945339
Andrey <andrey-mironov-96@mail.ru>
2020-05-03 09:39:55 +0400

    начало выполнения домащнего задания #7

---
+++ homework7/src/js/main.js
@@ -0,0 +1,63 @@
+//homework 6
+
+//task1
+let task1 = () =>{
+    let startCall = document.getElementById('start');
+    console.log(startCall);
+};
+task1();
+
+let task2 = () =>{
+    let budgetValue = document.getElementsByClassName('budget-value'),
+        levelValue = document.getElementsByClassName('level-value'),
+        expensesValue = document.getElementsByClassName('expenses-value'),
+        optionalexpensesValue = document.getElementsByClassName('optionalexpenses-value'),
+        incomeValue = document.getElementsByClassName('income-value'),
+        monthsavingsValue = document.getElementsByClassName('monthsavings-value'),
+        yearsavingsValue = document.getElementsByClassName('yearsavings-value'),
+        yearValue = document.getElementsByClassName('year-value'),
+        monthValue = document.getElementsByClassName('month-value'),
+        dayValue = document.getElementsByClassName('day-value'); 
+};
+task2();
+
+let task3 = () =>{
+    let expensesItem = document.getElementsByClassName('expenses-item');
+};
+task3();
+let task4 = ()=>{
+let expensesItemBtn1 = document.getElementsByTagName('button')[0],
+    expensesItemBtn2 = document.getElementsByTagName('button')[1],
+    expensesItemBtn3 = document.getElementsByTagName('button')[2];
+    console.log(expensesItemBtn1);
+    console.log(expensesItemBtn2);
+    console.log(expensesItemBtn3);
+    
+};
+task4();
+
+let task5 = ()=>{
+    let optionalexpensesItem = document.querySelectorAll('.optionalexpenses-item');
+    
+};
+
+let task6 = ()=>{
+    let  chooseIncome = document.querySelector('.choose-income'),
+         savings = document.querySelector('#savings'),
+         chooseSum = document.querySelector('.choose-sum'),
+         choosePercent = document.querySelector('.choose-percent'),
+         yearValue = document.querySelector('.year-value'),
+         monthValue = document.querySelector('.month-value'),
+         dayValue = document.querySelector('.day-value');
+    console.log(chooseIncome);
+    console.log(savings);
+    console.log(chooseSum);
+    console.log(choosePercent);
+    console.log(yearValue);
+    console.log(monthValue);
+    console.log(dayValue);
+};
+task6();
+
+
+
